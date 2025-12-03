89f7ee4d746022e4e39827fa067685b8fc917567
srushtims2108 <srushtims2005@gmail.com>
2025-12-03 15:47:54 +0530

    hello8

--- frontend/src/modules/socket.js
+++ frontend/src/modules/socket.js
@@ -1,11 +1,7 @@
-// socket.js
 import { io } from "socket.io-client";
 
-const BACKEND = process.env.REACT_APP_SERVER || "http://localhost:5000";
-
-const socket = io(BACKEND, {
-  transports: ["websocket", "polling"],
-  autoConnect: true,
+const socket = io(process.env.REACT_APP_SERVER || "https://hangman-backend-wsbz.onrender.com", {
+  transports: ["websocket"],
 });
 
 export { socket };
